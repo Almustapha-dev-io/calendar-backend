69af461f3324bc2838e4336f089a5ffd3d12004f
almustaphamuha98@gmail.com <almustaphamuha98@gmail.com>
2021-11-27 13:39:32 +0100

    Auth controller code refactor

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -16,15 +16,15 @@ export const signUp = async (req: Request, res: Response, next: NextFunction) =>
     try {
         let user = await User.findOne({ email: req.body.email.trim() });
         if (user) return res.status(422).json(response('A user with given email exists on our system!'));
-    
+
         user = new User(req.body);
-        
+
         const token = await genSalt(32);
         user.verificationToken = token;
-        
+
         await user.hashPassword();
         await user.save();
-    
+
         const data = _.pick(user, ['email', 'firstName', 'lastName', '_id']);
         res.status(201).json(response('Signup successful', data));
         sendMail(user.email, token, MailType.ACCOUNT_VERIFY);
@@ -40,15 +40,15 @@ export const signIn = async (req: Request, res: Response, next: NextFunction) =>
 
     try {
         const user = await User.findOne({ email: email.trim() });
-    
+
         if (!user) return res.status(400).json(response(errMsg));
-    
+
         const passwordValid = await user.comparePassword(password);
         if (!passwordValid) return res.status(400).json(response(errMsg));
         if (!user.verified) return res.status(401).json(response('Account email not verfied!'));
 
         const token = await user.generateAuthToken();
-        const userInfo = _.pick(user, ['firstName', 'lastName', 'email', 'verified']);  
+        const userInfo = _.pick(user, ['firstName', 'lastName', 'email', 'verified']);
         res.status(200).json(response('Login Successful', { token, userInfo }));
     } catch (err) {
         next(err);
@@ -65,7 +65,7 @@ export const verifyEmail = async (req: Request, res: Response, next: NextFunctio
         const update = { $set: { verificationToken: '', verified: true } };
         const user = await User.findOneAndUpdate(query, update, { new: true })
         if (!user) return res.status(400).json(response('Invalid verification token!'));
-    
+
         res.status(200).json(response('Email verified successfully!'));
     } catch (err) {
         next(err);
@@ -80,14 +80,14 @@ export const recoverPassword = async (req: Request, res: Response, next: NextFun
     try {
         const user = await User.findOne({ email });
         if (!user) return res.status(404).json(response('No users with email found'));
-    
+
         const token = await genSalt(32);
         user.passwordResetToken = token;
         user.passwordResetTokenExp = Date.now() + 3600000; // Expires in an hour
-    
+
         await user.save();
         res.status(200).json(response('Password reset request sent!'));
-    
+
         sendMail(user.email, token, MailType.PASSWORD_RESET);
     } catch (err) {
         next(err);
@@ -96,31 +96,31 @@ export const recoverPassword = async (req: Request, res: Response, next: NextFun
 
 
 export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
-    const { token, password, confirmPassword } = req.body;
-    if (!token) return res.status(400).json(response('Invalid token'));
-
-    const errors = validatePassword(
-        { label: 'Password', value: password },
-        { label: 'Confirm password', value: confirmPassword },
-    );
-    if (errors.length) {
-        const msg = errors[0].details[0].message;
-        return res.status(400).json(response(msg));
-    }
-
-    if (password !== confirmPassword) return res.status(422).json(response('Password and confirm password not same'));
-
     try {
-        const user = await User.findOne({ 
-            passwordResetToken: token, 
-            passwordResetTokenExp: { $gt: Date.now() } 
+        const { token, password, confirmPassword } = req.body;
+        if (!token) return res.status(400).json(response('Invalid token'));
+
+        const errors = validatePassword(
+            { label: 'Password', value: password },
+            { label: 'Confirm password', value: confirmPassword },
+        );
+        if (errors.length) {
+            const msg = errors[0].details[0].message;
+            return res.status(400).json(response(msg));
+        }
+
+        if (password !== confirmPassword) return res.status(422).json(response('Password and confirm password not same'));
+
+        const user = await User.findOne({
+            passwordResetToken: token,
+            passwordResetTokenExp: { $gt: Date.now() }
         });
         if (!user) return res.status(400).json(response('Invalid token or token expired!'));
 
         user.passwordResetToken = '';
         user.passwordResetTokenExp = 0;
         user.password = password;
-        
+
         await user.hashPassword();
         await user.save();
 
@@ -132,20 +132,19 @@ export const resetPassword = async (req: Request, res: Response, next: NextFunct
 
 
 export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
-    const { oldPassword, newPassword } = req.body;
-
-    const errors = validatePassword(
-        { label: 'New password', value: newPassword },
-        { label: 'Old password', value: oldPassword }
-    );
-    
-    if (errors.length) {
-        const msg = errors[0].details[0].message;
-        return res.status(400).json(response(msg));
-    }
-
-    const user = req.user;
     try {
+        const { oldPassword, newPassword } = req.body;
+        const errors = validatePassword(
+            { label: 'New password', value: newPassword },
+            { label: 'Old password', value: oldPassword }
+        );
+
+        if (errors.length) {
+            const msg = errors[0].details[0].message;
+            return res.status(400).json(response(msg));
+        }
+
+        const user = req.user;
         const oldPasswordValid = await user.comparePassword(oldPassword);
         if (!oldPasswordValid) return res.status(400).json(response('Your current password is incorrect!'));
 
