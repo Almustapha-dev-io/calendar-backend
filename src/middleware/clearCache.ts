666e8c89a5686de884fbee945fd3398b5e8f4957
Almustapha Ibrahim Muhammad <68342652+Almustapha-dev-io@users.noreply.github.com>
2022-11-11 22:05:00 +0100

    Delete clearCache.ts

--- src/middleware/clearCache.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-// import clearHash from '../util/cache';
-
-const clearCache = async (req: Request, res: Response, next: NextFunction) => {
-    await next();
-//     clearHash(req.user._id);
-};
-
-export default clearCache;
